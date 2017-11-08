2e14c954dbdbd00cf6bc2824343005bb2a45bc66
effyfan <31666940+effyfan@users.noreply.github.com>
2017-11-07 20:58:57 -0500

    Update sketch.js

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,99 +1,80 @@
-
 let url1 = 'https://api.mapbox.com/styles/v1/mapbox/light-v9/static/0,0,1,0,0/1024x512?access_token='
 /////////////////////////////////////////////////////////////////lat,long,zoom,angle,angle//////////
-let apiKey = 'pk.eyJ1IjoiZWZmeWZhbiIsImEiOiJjajkxdnNnZGIzZG1zMndtYmtiNTJzeXR6In0.I3-_XtcuL7WiF7eJZXIENw'
-let mapImg;
+let apiKey = 'pk.eyJ1IjoiZWZmeWZhbiIsImEiOiJjajh6N3psdGMwZjZtMzJvNGx5Ym54aTdkIn0.BcJIyPZ2FXZBbVWg71unOA'
 let emissions;
-//https://raw.githubusercontent.com/cvalenzuela/Mappa/master/tutorials/pollutionmap/data/co2_emissions.csv
 
-let clat = 0;
-let clng = 0;
-let lat;
-let lng;
-let mag;
 let zoom = 1;
 let particles = [];
-let emiinp;
+let myMap;
+let canvas;
+let mappa;
 
-function preload() {
-   mapImg = loadImage(url1 + apiKey)
-   emissions = loadStrings('./scripts/emissions2.csv',callbackEm);
+let options = {
+  lat: 0,
+  lng: 0,
+  zoom: 1,
+  style: 'mapbox://styles/effyfan/cj91vol42g93z2rmiqyhjfref'
 }
 
-function callbackEm(e){
+
+function preload() {
+  emissions = loadStrings('./scripts/emissions2.csv',callback);
+}
+
+function callback(e){
   console.log(e);
 }
 
-function mercX(lng) {
-  lng = radians(lng);
-  let a = (256 / PI) * pow(2, zoom);
-  let b = lng + PI;
-  return a * b;
-}
-
-
-function mercY(lat) {
-  lat = radians(lat);
-  let a = (256 / PI) * pow(2, zoom);
-  let b = tan(PI / 4 + lat / 2);
-  let c = PI - log(b);
-  return a * c;
-}
-
 function setup() {
-  createCanvas(1024, 512);
-  imageMode(CENTER);
-}
+  canvas = createCanvas(windowWidth, windowHeight);
+  mappa = new Mappa('Mapboxgl', apiKey);
+  myMap = mappa.tileMap(options);
+  myMap.overlay(canvas);
+  //myMap.onChange(drawPoints);
 
-function newInput(){
+  fill(70, 200, 200);
+  stroke(100);
+  imageMode(CENTER);
 }
 
 function draw() {
   clear();
-  push();
-  translate(width / 2, height / 2);
-  image(mapImg, 0, 0);
-  let cx = mercX(clng);
-  let cy = mercY(clat);
+  // draw red circles indicate emissions amount
+    for (let i = 0; i < emissions.length; i++) {
+      let data = emissions[i].split(",");
+      let latitude = data[1];
+      let longitude = data[2];
+      let co2 = data[3];
+      let popl = data[4];
+      let area = co2 * 100000 / popl
+      console.log(area);
+      let d = sqrt(area/PI);
 
-// draw red circles indicate emissions amount
-  for (let i = 0; i < emissions.length; i++) {
-    let data = emissions[i].split(/,/);
-    let lat = data[1];
-    let lng = data[2];
-    let co2 = data[3];
-    let popl = data[4];
-    let x = mercX(lng) - cx;
-    let y = mercY(lat) - cy;
-    let area = co2*100000/popl;
-    //console.log(area);
-    let d = sqrt(area/PI);
-    noStroke();
-    fill(255, 0, 0, 100);
-    ellipse(x, y, d);
+      noStroke();
+      fill(255, 0, 0, 100);
+      let pos = myMap.latLngToPixel(latitude, longitude);
+      ellipse(pos.x, pos.y, d, d);
 
-// draw emission animation
-    if (area > emiinp){
-      for (let j = 0; j < 1; j++) {
-      let p = new Particle(x, y, d/5);
-      particles.push(p);
-      }
-      for (let j = particles.length - 1; j >= 0; j--) {
-      particles[j].update();
-      particles[j].show();
-      if (particles[j].finished()) {
-      // remove this particle
-      particles.splice(j, 1);
+      // draw emission animation
+      if (area > emiinp){
+       for (let j = 0; j < 1; j++) {
+          let p = new Particle(pos.x, pos.y, d/5);
+          particles.push(p);
+       }
+       for (let j = particles.length - 1; j >= 0; j--) {
+          particles[j].update();
+          particles[j].show();
+          if (particles[j].finished()) {
+          // remove this particle
+          particles.splice(j, 1);
+       }
       }
     }
   }
-  }
-  pop();
 }
 
 
 class Particle {
-
   constructor(a, b, dia) {
     this.x = a;
     this.y = b;
@@ -115,8 +96,8 @@ class Particle {
 
   show() {
     noStroke();
+    //stroke(255);
     fill(100, this.alpha);
     ellipse(this.x, this.y, this.dia);
   }
-
 }
